8300c6ee7c5495d5fdfffa06099210138f0d8012
DaisukeYamano <dai.biker@gmail.com>
2014-03-02 17:30:23 +0900

    Add modified

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,32 +4,56 @@ feature "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    scenario { should have_content(heading) }
+    scenario { should have_title(full_title(page_title)) }
+  end
+
   context "Home pages" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    scenario { should have_content('Sample App') }
-    scenario { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     scenario { should_not have_title('| Home') }
   end
 
   context "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
     
-    scenario { should have_content('Help') }
-    scenario { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
   
   context "About page" do
     before { visit about_path }
-
-    scenario { should have_content('About Us') }
-    scenario { should have_title(full_title('About')) }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About' }
+    
+    it_should_behave_like "all static pages"
   end
   
   context "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+    
+    it_should_behave_like "all static pages"
+  end
 
-    scenario { should have_content('Contact') }
-    scenario { should have_title(full_title('Contact')) }
+  scenario "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
